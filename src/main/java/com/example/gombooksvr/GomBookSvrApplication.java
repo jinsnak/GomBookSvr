22825f609cc219d82ceffe87bd48d7d82e9a4ebe
BumjinPark <jinsnak@gmail.com>
2025-01-14 15:53:25 +0900

    Initial commit

---
+++ src/main/java/com/example/gombooksvr/GomBookSvrApplication.java
@@ -0,0 +1,13 @@
+package com.example.gombooksvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomBookSvrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GomBookSvrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gombooksvr/GomBookSvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gombooksvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomBookSvrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
